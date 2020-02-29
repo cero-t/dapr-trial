8b34f9592fa056bd97565de86ff84dbdb5be7a7d
Shin Tanimoto <shin@tanimo.to>
2020-02-29 17:36:51 +0900

    rename

--- dapr-remote/src/main/java/ninja/cero/example/dapr/RemoteApplication.java
+++ dapr-remote/src/main/java/ninja/cero/example/dapr/RemoteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DaprApplication {
+public class RemoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DaprApplication.class, args);
+		SpringApplication.run(RemoteApplication.class, args);
 	}
 
 }
